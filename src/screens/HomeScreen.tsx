4436907fbc27d8fb877d5b61faf22c5de9a2cf6d
Luis <luismepa@gmail.com>
2019-06-24 00:09:53 -0500

    Pelisflix search bar

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {inject, observer} from "mobx-react";
-import {Container, Header, Content, Tab, Tabs, Text, ScrollableTab} from 'native-base';
+import {Container, Header, Content, Tab, Tabs, Text, ScrollableTab, Item, Icon, Input, Button} from 'native-base';
 import MovieGridList from '../components/MovieGridList'
 
 interface HomeScreenInterface {
@@ -11,12 +11,39 @@ interface HomeScreenInterface {
 @observer
 class HomeScreen extends React.Component<HomeScreenInterface> {
 
+  state = {
+    filter: '',
+    movies: [],
+  };
+
+  componentDidMount(): void {
+    this._filterBySearchTerm('');
+  }
+
+  /**
+   * Filter
+   * @private
+   */
+  _filterBySearchTerm(filter) {
+    const movies = this.props.rootStore
+      .movieStore
+      .movies
+      .filter(m => m.title.indexOf(filter) >= 0);
+
+    this.setState({
+      filter,
+      movies
+    });
+  }
+
+  /**
+   * Tabs
+   * @private
+   */
   _renderTabs() {
     return this.props.rootStore.movieStore.genres.map(genre => {
       // console.log(this.props.rootStore.movieStore.movies[0]);
-      const movies = this.props.rootStore
-        .movieStore
-        .movies
+      const movies = this.state.movies
         .filter(m => m.genres.find(g => g.id === genre.id));
 
       return (
@@ -33,7 +60,22 @@ class HomeScreen extends React.Component<HomeScreenInterface> {
   render() {
     return (
       <Container>
+        <Header searchBar rounded>
+          <Item>
+            <Icon name="ios-search" />
+            <Input placeholder="Buscar" onChangeText={text => this._filterBySearchTerm(text)} />
+          </Item>
+          <Button transparent>
+            <Text>Search</Text>
+          </Button>
+        </Header>
         <Tabs renderTabBar={() => <ScrollableTab />}>
+          <Tab heading={'Todos'}>
+            <MovieGridList
+              navigation={this.props.navigation}
+              movies={this.state.movies}
+            />
+          </Tab>
           {this._renderTabs()}
         </Tabs>
       </Container>
